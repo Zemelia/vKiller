6ee81470f5375c387ae1dce8950d8feb792dd65a
Oleg Zemelia <ol.raban@gmail.com>
2015-04-05 23:42:45 +0300

    9

--- client/views/friends/friends.js
+++ client/views/friends/friends.js
@@ -1,28 +1,26 @@
 Template.friends.events({
-  'click .follow': function () {
-    console.log(this)
-    if (this._id) {
-      console.log(this._id)
-      //Meteor.call('followUser', Meteor.userId(), this._id, true);
+  'click a.follow': function (e) {
+    var userId = _.last(e.currentTarget.href.split('/'));
+    if (userId) {
+      Meteor.call('followUser', Meteor.userId(), userId, true);
     }
     return false;
   },
-  'click .unfollow': function () {
-    console.log(this)
-    if (this._id) {
-      console.log(this._id)
-      //Meteor.call('followUser', Meteor.userId(), this._id, false);
+  'click .unfollow': function (e) {
+    var userId = _.last(e.currentTarget.href.split('/'));
+    if (userId) {
+      Meteor.call('followUser', Meteor.userId(), userId, false);
     }
     return false;
   }
 });
 
-Template.people_item.helpers({
+Template.friends.helpers({
   followLink: function () {
     var user = Meteor.user();
     if (checkObjectExists(user, 'friends.followed') && _.contains(user.friends.followed, this._id)) {
-      return Blaze._globalHelpers.linkGenerate("Unfollow", this._id, "unfollow")
+      return Blaze._globalHelpers.linkGenerate("Unfollow", 'follow/' + this._id, "unfollow")
     }
-    return Blaze._globalHelpers.linkGenerate("add friend", this._id, "follow")
+    return Blaze._globalHelpers.linkGenerate("add friend", 'follow/' + this._id, "follow")
   }
 });
--- server/publications.js
+++ server/publications.js
@@ -2,7 +2,39 @@ Meteor.publish('posts', function(options) {
   return Posts.find({}, options);
 });
 Meteor.publish('users', function(options) {
-  return Meteor.users.find({}, options);
+  if (options && options.friends) {
+    switch (options.friends) {
+      case 'activeFriends':
+      case 'followers':
+      case 'followed':
+        var query = {
+          "_id": this.userId
+        };
+        query["friends." + options.friends] = {$exists: 1};
+        var projection = {
+          fields: {
+            "_id": 0
+          }
+        };
+        projection['fields']["friends." + options.friends] = 1;
+        var friends = Meteor.users.findOne(query, projection);
+        if (friends) {
+          var idList = friends.friends[options.friends];
+          if (options.friends == 'followers') {
+            idList = [];
+            _.each(friends.friends[options.friends], function (i, j) {
+              if (i['id']) {
+                idList.push(i['id']);
+              }
+            })
+          }
+          idList.push(this.userId);
+          return Users.find({"_id": {$in: idList}}, {});
+        }
+        return;
+    }
+  }
+  return Meteor.users.find({});
 });
 Meteor.publish('chatroom', function() {
   return chatRoom.find({recipients: this.userId});
